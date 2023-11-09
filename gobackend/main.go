fad9d82ca307e0b51ec8fcab6acf169b47dbb8e4
Drew Schillinger <drew@doctorew.com>
2023-11-09 15:29:59 -0500

    switching backend to golang since bun does not play well with apollo just yet

---
+++ gobackend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"log"
+	"net/http"
+	"your_project/graph"
+	"your_project/graph/generated"
+)
+
+func main() {
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
