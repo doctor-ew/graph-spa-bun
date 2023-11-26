eaef537299cbaec5d1ad77e74bf3b05b32ff2369
Drew Schillinger <drew@doctorew.com>
2023-11-26 12:31:36 -0500

    Resolver not working

--- backend/src/graphql/rickmorty/resolvers.ts
+++ backend/src/graphql/rickmorty/resolvers.ts
@@ -2,34 +2,20 @@
 
 import axios from 'axios';
 
+
 // Helper function to determine if a Rick and Morty should be associated
 const shouldAssociate = (rick: any, morty: any) => {
-    //console.log(`|-SA-| 0) Checking association for Rick: ${JSON.stringify(rick)} :: Morty: ${JSON.stringify(morty)}`);
-
-    // Match by origin id
     if (rick.origin?.id && morty.origin?.id && rick.origin.id === morty.origin.id) {
-        // console.log(`|-SA-| 1) Comparing origin IDs: Rick: ${rick.origin.id} :: Morty: ${morty.origin.id}`);
         return true;
     }
-
-    // If origin is unknown, match by name pattern (e.g. Alien Rick and Alien Morty)
     if (rick.origin?.name === 'unknown' && morty.origin?.name === 'unknown') {
         const rickNamePattern = rick.name.replace('Rick', '').trim();
         const mortyNamePattern = morty.name.replace('Morty', '').trim();
-
-        if (rickNamePattern && mortyNamePattern && rickNamePattern === mortyNamePattern) {
-            // console.log(`|-SA-| 2) Comparing name patterns: Rick: ${rickNamePattern} :: Morty: ${mortyNamePattern}`);
-            return true;
-        }
+        return rickNamePattern === mortyNamePattern;
     }
-
-    // Match by location if both locations are known and not the Citadel of Ricks
     if (rick.location?.id && morty.location?.id && rick.location.id === morty.location.id && rick.location.id !== '3') {
-        // console.log(`|-SA-| 3) Comparing location IDs: Rick: ${rick.location.id} - ${rick.location.name} :: Morty: ${morty.location.id} - ${morty.location.name}`);
         return true;
     }
-
-    // console.log(`|-SA-| 4) No match found for Rick: ${rick.name} :: Morty: ${morty.name}`);
     return false;
 };
 
@@ -45,16 +31,22 @@ type PocketNPCData = {
     npc_name: string;
 };
 
+// Interface for Morty objects from the Rick and Morty API
+interface Morty {
+    id: string;
+    name: string;
+    // ... other fields that Morty objects contain
+}
+
+type UnifiedMorty = Morty & PocketMortyData;
+
 const rickMortyResolvers = {
     Query: {
-        // Resolves characters by name
         charactersByName: async (_: any, args: { name: string }) => {
-            // GraphQL query to fetch characters by name
             const query = `
                 {
                     characters(filter: {name: "${args.name}"}) {
                         results {
-                            // Fields to retrieve for each character
                             id
                             name
                             status
@@ -73,99 +65,71 @@ const rickMortyResolvers = {
                         }
                     }
                 }`;
-
             try {
-                // Making a POST request to the Rick and Morty GraphQL API
                 const response = await axios.post('https://rickandmortyapi.com/graphql', {query});
-                // Returning the results from the response
                 return response.data.data.characters.results;
             } catch (error) {
-                // Handling errors in fetching characters
                 console.error("Error fetching characters:", error);
                 return [];
             }
         },
 
-        // Resolves associations between Ricks and Morties
         rickAndMortyAssociations: async () => {
             try {
-                // Fetching all characters named 'Rick'
                 const ricksResponse = await axios.get('https://rickandmortyapi.com/api/character/?name=Rick');
-                // Fetching all characters named 'Morty'
                 const mortiesResponse = await axios.get('https://rickandmortyapi.com/api/character/?name=Morty');
-
-                // Extracting results from responses
                 const ricks = ricksResponse.data.results;
                 const morties = mortiesResponse.data.results;
 
-                // Mapping each Rick to an associated Morty
-                return ricks.map((rick: any) => {
-                    // Finding an associated Morty for each Rick
-                    const associatedMorty = morties.find((morty: any) => shouldAssociate(rick, morty));
-                    // Returning the Rick and any associated Morty
-                    return {
-                        rick,
-                        morties: associatedMorty ? [associatedMorty] : []
-                    };
+                const pocketMortysResponse = await axios.get('https://pocketmortys.net/components/com_pocketmortys/json/datatables/MortysTable_en.json');
+                const pocketNPCsResponse = await axios.get('https://pocketmortys.net/components/com_pocketmortys/json/datatables/NPCsTable_en.json');
+                const pocketMortys = pocketMortysResponse.data.data;
+                const pocketNPCs = pocketNPCsResponse.data.data;
+
+                const pocketAssociations = pocketNPCs.map((npc: any) => {
+                    const rickAlias = npc.alias.replace(/-/g, ' ');
+                    const associatedMorties = pocketMortys.filter((morty: any) => {
+                        const mortyAlias = morty.alias.replace(/^\d+-/, '').replace(/-/g, ' ');
+                        return mortyAlias.includes(rickAlias);
+                    });
+                    return {rick: npc, morties: associatedMorties};
                 });
+
+                const combinedAssociations = [...ricks.map((rick: any) => {
+                    const associatedMorty = morties.find((morty: any) => shouldAssociate(rick, morty));
+                    return {rick, morties: associatedMorty ? [associatedMorty] : []};
+                }), ...pocketAssociations];
+
+                // Adding orphaned Morties (those without an associated Rick)
+                const orphanedMorties = morties.filter((morty: Morty) => {
+                    return !combinedAssociations.some(association =>
+                        association.morties.some((m: UnifiedMorty) => m.id === morty.id)
+                    );
+                }).map(morty => ({ id: morty.id, name: morty.name }));
+
+// Adding orphaned Morties from PocketMortys
+                const orphanedPocketMorties = pocketMortys.filter((pocketMorty: PocketMortyData) => {
+                    return !combinedAssociations.some(association =>
+                        association.morties.some((m: UnifiedMorty) => m.assetid === pocketMorty.assetid)
+                    );
+                }).map(pocketMorty => ({ id: pocketMorty.assetid, name: pocketMorty.name }));
+
+
+
+                return {
+                    associations: combinedAssociations,
+                    orphanedMorties: [...orphanedMorties, ...orphanedPocketMorties]
+                };
+
+
+
             } catch (error) {
-                // Handling errors in rickAndMortyAssociations query
                 console.error("Error in rickAndMortyAssociations:", error);
-                return [];
-            }
-        },
-        pocketMortys: async () => {
-            try {
-                const response = await axios.get('https://pocketmortys.net/components/com_pocketmortys/json/datatables/MortysTable_en.json');
-                return response?.data?.data?.map((pocketMorty: PocketMortyData) => ({
-                    assetid: pocketMorty.assetid,
-                    alias: pocketMorty.alias,
-                    dimensions: pocketMorty.dimensions,
-                    name: pocketMorty.name
-                }));
-            } catch (error) {
-                console.error("Error fetching PocketMortys data:", error);
-                return [];
+                return {associations: [], orphanedMorties: []};
             }
         },
 
-        // New resolver to fetch a specific Morty by assetid
-        pocketMorty: async (_: any, args: { assetid: string }) => {
-            try {
-                const response = await axios.get('https://pocketmortys.net/components/com_pocketmortys/json/datatables/MortysTable_en.json');
-                return response?.data?.data?.find((pocketMorty: PocketMortyData) => pocketMorty.assetid === args.assetid);
-            } catch (error) {
-                console.error("Error fetching Morty:", error);
-                return null;
-            }
-        },
-
-        // New resolver to fetch NPCs data
-        pocketNpcs: async () => {
-            try {
-                const response = await axios.get('https://pocketmortys.net/components/com_pocketmortys/json/datatables/NPCsTable_en.json');
-                return response?.data?.data?.map((pocketnpc: PocketNPCData) => ({
-                    alias: pocketnpc.alias,
-                    npc_name: pocketnpc.npc_name
-                }));
-            } catch (error) {
-                console.error("Error fetching NPCs data:", error);
-                return [];
-            }
-        },
-
-        // New resolver to fetch a specific NPC by alias
-        pocketNpc: async (_: any, args: { alias: string }) => {
-            try {
-                const response = await axios.get('https://pocketmortys.net/components/com_pocketmortys/json/datatables/NPCsTable_en.json');
-                return response.data.data.find((pocketNpc: PocketNPCData) => pocketNpc.alias === args.alias);
-            } catch (error) {
-                console.error("Error fetching NPC:", error);
-                return null;
-            }
-        },
-    }
-
+        // ... other resolvers, such as pocketMortys, pocketMorty, pocketNpcs, pocketNpc
     }
 };
 
